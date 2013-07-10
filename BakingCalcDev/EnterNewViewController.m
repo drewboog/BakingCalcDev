40eed083f5affe571fcedb5b4a68e4a81eeb8448
drewboog <hazlet26@gmail.com>
2013-07-10 14:55:05 -0700

    Cleaned up NewEntryVC

--- BakingCalcDev/EnterNewViewController.m
+++ BakingCalcDev/EnterNewViewController.m
@@ -29,6 +29,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    self.view..view.backgroundcolor = [UIColor blueColor];
 }
 
 - (void)didReceiveMemoryWarning
@@ -41,65 +42,4 @@
 
 
 
-
-
-@implementation EnterNewViewController
-
-- (id) initWithNibName:(NSString *)nibNameOrNil // ...
-
-// Custom init:
-self.title = @"New Recipe";
-self.tabBarItem.image = [UIImage imageNamed:@"tab_icon_new"];
-
-
-...
-
-
-
--(void) viewDidLoad
-{
-    ...
-    
-    self.view..view.backgroundcolor = [UIColor blueColor];
-    
-    ...
-    
-    
-    
-    // Different file!
-    @implementation savedEntryViewController
-    
-    - (id) initWithNibName:(NSString *)nibNameOrNil // ...
-    
-    // Custom init:
-    self.title = @"Recipes";
-    self.tabBarItem.image = [UIImage imageNamed:@"tab_icon_saved"];
-    
-    
-    ...
-    
-    
-    
-    -(void) viewDidLoad
-    {
-        ...
-        
-        self.view.view.backgroundcolor = [UIColor yellowColor];
-        
-        UIImageView *recipeImageView = [[UIImageView alloc] initWithImage:
-                                        [UIImage imageNamed:@"recipe.jpg"]];
-        
-        
-        recipeImageView.frame = CGRectMake(20,20,100,114);
-        [self.view addSubview:recipeImageView];
-        
-        
-        UILabel *nameLabel = [[UILabel alloc] initWithFrame:CGRectMake(20,140,280,40);
-                              // Fonts and formatting slide 85:
-                              nameLabel.text = @"Recipe: Punkin pi";
-                              [self.view addSubview:nameLabel];
-                              
-                              // Scrolling on slide 87
-                              
-                              ...
 
