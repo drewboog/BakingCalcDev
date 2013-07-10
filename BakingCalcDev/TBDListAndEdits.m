4d83b4af764288a91c36f32e1f3c055e9e693c30
drewboog <hazlet26@gmail.com>
2013-07-10 15:50:34 -0700

    7/10 to do list

---
+++ BakingCalcDev/TBDListAndEdits.m
@@ -0,0 +1,17 @@
+
+TBD list and snippits:
+
+7/10
+  - Add recipe.jpg to files
+  - Compile code, fix mistakes.. 
+  - Clean up commented out code.
+  
+  - Add logo picture to files
+  - Set up to show logo at start
+  
+  - TableViewController to be added for recipe list (slide 111)
+    - Create new file
+
+
+
+Table view code:
