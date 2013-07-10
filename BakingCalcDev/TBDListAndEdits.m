d43f7f0c733dc1901ed48ab9080dac2df54efae0
drewboog <hazlet26@gmail.com>
2013-07-10 16:05:22 -0700

    getting started with table view code

--- BakingCalcDev/TBDListAndEdits.m
+++ BakingCalcDev/TBDListAndEdits.m
@@ -1,10 +1,10 @@
-
 TBD list and snippits:
 
 7/10
   - Add recipe.jpg to files
   - Compile code, fix mistakes.. 
   - Clean up commented out code.
+  - Add #import to view controller 
   
   - Add logo picture to files
   - Set up to show logo at start
@@ -15,3 +15,19 @@ TBD list and snippits:
 
 
 Table view code:
+
+-- AppDelegate.m -- (? - or add where needed)
+
+#import "RecipeTableViewController.h"
+
+...
+{
+  RecipeTableViewController *recipeTableViewController = [[recipeTableViewController alloc] 
+                                                           initWithStyle:UITableViewStyleGrouped];
+
+
+
+-- RecipeTableViewController.m -- 
+
+// Need to add number of sections (=1), number of rows, init each cell (slide 116)
+// Not sure what is in the defalt code for this.. 
