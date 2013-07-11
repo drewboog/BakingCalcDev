b552a68503c25171cf995c03c9e0e7d0c16bd36d
drewboog <hazlet26@gmail.com>
2013-07-11 16:59:02 -0700

    update list

--- BakingCalcDev/TBDListAndEdits.m
+++ BakingCalcDev/TBDListAndEdits.m
@@ -1,36 +1,26 @@
 TBD list and snippits:
 
+
+
+7/11
+  - How do I store data?
+  
+  - Add a photo view controller
+  
+  - Add logo picture to files
+  - Set up to show logo at start
+  
+  
+  
 7/10
   - Add recipe.jpg to files - done
   - Compile code, fix mistakes.. - done
   - Clean up commented out code.- done
   - Add #import to view controller - done
   
-
-  - TableViewController to be added for recipe list (slide 111)
+  - TableViewController to be added for recipe list (slide 111) -done
     - Create new file - done
     
   - Add logo picture to files
   - Set up to show logo at start
 
-
-
-
-
-Table view code:
-
--- AppDelegate.m -- (? - or add where needed)
-
-#import "RecipeTableViewController.h"
-
-...
-{
-  RecipeTableViewController *recipeTableViewController = [[recipeTableViewController alloc] 
-                                                           initWithStyle:UITableViewStyleGrouped];
-
-
-
--- RecipeTableViewController.m -- 
-
-// Need to add number of sections (=1), number of rows, init each cell (slide 116)
-// Not sure what is in the defalt code for this.. 
